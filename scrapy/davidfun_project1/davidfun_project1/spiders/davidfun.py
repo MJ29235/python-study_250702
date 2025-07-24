1e57b01a2a48a3916525fe872d8522046cbf1312
MJ29235 <pmj960128@gmail.com>
2025-07-24 17:59:22 +0900

    selenium"scrapy

---
+++ scrapy/davidfun_project1/davidfun_project1/spiders/davidfun.py
@@ -0,0 +1,11 @@
+import scrapy
+
+
+class DavidfunSpider(scrapy.Spider):
+    name = "davidfun"
+    allowed_domains = ["davelee-fun.github.io"]
+    start_urls = ["https://davelee-fun.github.io/"]
+
+    def parse(self, response):
+        
+        pass
